cfa70cb42b80c9976258b0a4ed94a3c24b569c91
Giovanni Volpi <giovannivolpi@mail.weber.edu>
2024-09-05 11:01:59 -0600

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 
 int main(){
     // TODO
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "What score did oyu get for Notebook1" << std::endl;
+    double score;
+    std::cin >> score;
+
+    std::cout << score;
     
     return 0;
 }
