5eb98a90c89dcf3bca1d9d38de435d1d2de8713d
Giovanni Volpi <giovannivolpi@mail.weber.edu>
2024-09-13 19:09:23 +0000

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
-using namespace std;
 
 int main(){
     // TODO
-    cout << "What score did oyu get for Notebook1";
-    double score;
-    cin >> score;
-
-    char grade;
-    if(score >= 9){
-        grade = 'A';
-    } else if(score >= 8){
-        grade = 'B';
-    } else if(score >= 7){
-        grade = 'C';
-    } else {
-        grade = 'D';
-    }
-    cout << "A score of" << score << "means" << grade << endl;
+    std::cout << "Hello World!" << std::endl;
     
     return 0;
 }
